bb7b5b948ee4ab0d314d3e95e52bf32891bc21f8
Karim <karem109k@gmail.com>
2023-05-28 05:37:23 +0300

    remove unnessasary

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ console.log(controls.clientHeight)
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-console.log(ctx);
 let particlesArr = [];
 
 canvas.width = window.innerWidth;
